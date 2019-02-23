f447b0f152c641ed78adf6227008aaf1c3cb9368
clau <czumaran@icluster.cl>
2019-02-23 01:31:13 -0300

    modificacion lista

--- proyecto-vue/src/components/Datos.vue
+++ proyecto-vue/src/components/Datos.vue
@@ -1,81 +1,136 @@
 <template>
+  <div class="container">
     <div class="text-center">
-       
-        <img v-bind:src="imagenUsuario" rounded="circle" alt="Circle image"/>
-        <!-- <b-img v-bind="imagenUsuario" rounded="circle" alt="Circle image" /> -->
+      <img v-bind:src="imagenUsuario" rounded="circle" alt="Circle image">
+      <!-- <b-list-group v-if="listaDatosUsuario.length > 0" class="borderless" >
+        <b-list-group-item>
+            <img v-bind:src="imagenUsuario" rounded="circle" alt="Circle image">
+        </b-list-group-item>
+        <b-list-group-item>
+          <font-awesome-icon icon="user"/>
+          {{listaDatosUsuario[0].texto}}
+        </b-list-group-item>
+        <b-list-group-item>
+          <font-awesome-icon icon="envelope"/>
+          {{listaDatosUsuario[1].texto}}
+        </b-list-group-item>
+        <b-list-group-item>
+          <font-awesome-icon icon="calendar-alt"/>
+          {{listaDatosUsuario[2].texto}}
+        </b-list-group-item>
+        <b-list-group-item>
+          <font-awesome-icon icon="map-marked-alt"/>
+          {{listaDatosUsuario[3].texto}}
+        </b-list-group-item>
+        <b-list-group-item>
+          <font-awesome-icon icon="phone"/>
+          {{listaDatosUsuario[4].texto}}
+        </b-list-group-item>
+        <b-list-group-item>
+          <font-awesome-icon icon="lock"/>
+          {{listaDatosUsuario[5].texto}}
+        </b-list-group-item>
+<b-list-group-item>
 
-<b-list-group v-if="listaDatosUsuario.length > 0">
-    <b-list-group-item >  <font-awesome-icon icon="user" />  {{listaDatosUsuario[0].texto}} </b-list-group-item>
-    <b-list-group-item>  <font-awesome-icon icon="envelope" />  {{listaDatosUsuario[1].texto}} </b-list-group-item>
-    <b-list-group-item>  <font-awesome-icon icon="calendar-alt" />  {{listaDatosUsuario[2].texto}} </b-list-group-item>
-    <b-list-group-item>  <font-awesome-icon icon="map-marked-alt" />  {{listaDatosUsuario[3].texto}} </b-list-group-item>
-    <b-list-group-item>  <font-awesome-icon icon="phone" />  {{listaDatosUsuario[4].texto}} </b-list-group-item>
-    <b-list-group-item>  <font-awesome-icon icon="lock" />  {{listaDatosUsuario[5].texto}} </b-list-group-item>
-</b-list-group>
-<b-button @click="resetUsuario" >Button</b-button>
+      <b-button @click="resetUsuario">Button</b-button>
+</b-list-group-item>
 
+      </b-list-group>-->
+      <!-- <ul class="list-group " style="vertical-align: middle">
+        <li
+          v-for="item of listaDatosUsuario"
+          :key="item.id"
+          class="list-group-item"
+        >
+          <i class="material-icons">{{item.icono}}</i>
+          {{item.texto}}
+        </li>
+      </ul>-->
+      <b-list-group>
+        <b-list-group-item v-for="item of listaDatosUsuario" :key="item.id">
+          <div class="  ">
+            <i class="material-icons">{{item.icono}}</i>
+            {{item.texto}}
+          </div>
+        </b-list-group-item>
+      </b-list-group>
+
+      <b-button @click="resetUsuario">Button</b-button>
     </div>
+  </div>
 </template>
 
 
 <script>
-
-import axios from 'axios'
+import axios from "axios";
 
 export default {
-    name: 'Datos',
+  name: "Datos",
 
-    data(){
-        return{
-            dataUsuario:{},
-            imagenUsuario:[],
-            nombreCompletoUsuario:[],
-            listaDatosUsuario:[]
-        }
+  data() {
+    return {
+      dataUsuario: {},
+      imagenUsuario: [],
+      nombreCompletoUsuario: [],
+      listaDatosUsuario: [],
+      android: "android"
+    };
+  },
+
+  mounted() {
+    var self = this;
+    self.resetUsuario();
+  },
+  methods: {
+    resetUsuario: function() {
+      var self = this;
+      axios
+        .get("https://randomuser.me/api/")
+        .then(function(res) {
+          self.listaDatosUsuario = [];
+          self.dataUsuario = res.data.results[0];
+          self.imagenUsuario = self.dataUsuario.picture.large;
+          self.listaDatosUsuario.push({
+            icono: "person",
+            texto:
+              self.dataUsuario.name.title +
+              " " +
+              self.dataUsuario.name.first +
+              " " +
+              self.dataUsuario.name.last
+          });
+          self.listaDatosUsuario.push({
+            icono: "email",
+            texto: self.dataUsuario.email
+          });
+          self.listaDatosUsuario.push({
+            icono: "event",
+            texto: self.dataUsuario.dob.date
+          });
+          self.listaDatosUsuario.push({
+            icono: "place",
+            texto: self.dataUsuario.location.street
+          });
+          self.listaDatosUsuario.push({
+            icono: "local_phone",
+            texto: self.dataUsuario.phone
+          });
+          self.listaDatosUsuario.push({
+            icono: "lock",
+            texto: self.dataUsuario.login.password
+          });
+          console.log("Data: ", self.dataUsuario);
+        })
+        .catch(function(error) {
+          console.log("Error: ", error);
+        });
     },
-
-    mounted(){
-        var self = this;
-        self.resetUsuario();
-        // var self = this;
-        // axios.get('https://randomuser.me/api/')
-        // .then(function(res){
-        //     self.dataUsuario = res.data.results[0];
-        //     self.imagenUsuario = self.dataUsuario.picture.large;
-        //     self.listaDatosUsuario.push({"icono":"user", "texto":self.dataUsuario.name.title + " " + self.dataUsuario.name.first + " " + self.dataUsuario.name.last});
-        //     self.listaDatosUsuario.push({"icono":"bb", "texto":self.dataUsuario.email});
-        //     self.listaDatosUsuario.push({"icono":"bb", "texto":self.dataUsuario.dob.date});
-        //     self.listaDatosUsuario.push({"icono":"bb", "texto":self.dataUsuario.location.street});
-        //     self.listaDatosUsuario.push({"icono":"bb", "texto":self.dataUsuario.phone});
-        //     self.listaDatosUsuario.push({"icono":"bb", "texto":self.dataUsuario.login.password});
-
-        //     console.log('Data: ',res.data.results[0]);
-        // })
-        // .catch (function(error){
-        //     console.log('Error: ', error);
-        // })
-    },
-    methods: {
-        resetUsuario: function () {
-            var self = this;
-            axios.get('https://randomuser.me/api/')
-            .then(function(res){
-                self.listaDatosUsuario = [];
-                self.dataUsuario = res.data.results[0];
-                self.imagenUsuario = self.dataUsuario.picture.large;
-                self.listaDatosUsuario.push({"icono":"user", "texto":self.dataUsuario.name.title + " " + self.dataUsuario.name.first + " " + self.dataUsuario.name.last});
-                self.listaDatosUsuario.push({"icono":"envelope", "texto":self.dataUsuario.email});
-                self.listaDatosUsuario.push({"icono":"calendar-alt", "texto":self.dataUsuario.dob.date});
-                self.listaDatosUsuario.push({"icono":"map-marked-alt", "texto":self.dataUsuario.location.street});
-                self.listaDatosUsuario.push({"icono":"phone", "texto":self.dataUsuario.phone});
-                self.listaDatosUsuario.push({"icono":"lock", "texto":self.dataUsuario.login.password});
-
-                console.log('Data: ',self.dataUsuario);
-            })
-            .catch (function(error){
-                console.log('Error: ', error);
-            })
+    andres: function() {
+      console.log("variable");
+      return "user";
     }
   }
-}
+};
 </script>
+
+
--- proyecto-vue/src/components/Usuario.vue
+++ proyecto-vue/src/components/Usuario.vue
@@ -1,10 +1,12 @@
 <template>
+<div class="container">
     <div class="text-center">
-       
         <h1>
             Usuario Random
         </h1>
     </div>
+
+</div>
 </template>
 
 
